c5da1e4682d85587d9ab230b079b32a49bbed894
Ashley Richmond <luigi.sexpistols@gmail.com>
2025-07-21 23:35:16 +1000

    Updated script for setting scheme based on currently playing album's art.

--- assets/.config/rmpc/on-song-change.d/update-color-scheme.sh
+++ assets/.config/rmpc/on-song-change.d/update-color-scheme.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+set -e
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'on_exit $? $LINENO' EXIT
+on_exit() { [ $1 -ne 0 ] && x-log "Failed command (code $1) on line $2: '${last_command}'"; }
+
 home_dir='/home/ashley'
 config_dir="$home_dir"/.config/rmpc
 art_dir="$home_dir"/.cache/rmpc/art
@@ -25,10 +30,28 @@ if [ ! -f "$art_file" ]; then
     fi
 fi
 
-# for some reason the `wal` command is causing a frequent but intermittent issue in rmpc.
-# leave this enabled for long and you'll see it
-wal -nste -i "${art_file}" --backend=colorz
+x-log "Using album art file: $art_file"
+
+# try each backend until one succeeds; we prefer the schemes `colorz` generates, but fall back to `wal` in rare cases
+for b in colorz wal; do
+  x-log "Trying color scheme update with backend '$b'"
+
+  # `wal` must be run with the `-e` flag to ensure it doesn't interfere with rmpc
+  if wal -ne -i "${art_file}" --backend="$b"; then
+    x-log "Color scheme updated using backend '$b'"
+    break
+  fi
+done
 
 # reload theme
-theme=$(cat "$config_dir"/config.ron | grep 'theme' | grep -Eo '"[^"]+"' | grep -Eo '[^"]+')
-rmpc remote set theme "${config_dir}/themes/${theme}.ron" || rmpc remote --pid "$PID" status "Failed to set theme '${theme}'." --level error
+theme="$(cat "$config_dir"/config.ron | grep 'theme' | grep -Eo '"[^"]+"' | grep -Eo '[^"]+')"
+x-log "Theme to set: ${theme} @ ${config_dir}/themes/${theme}.ron"
+
+if rmpc remote set theme "${config_dir}/themes/${theme}.ron"; then
+  pywalfox update
+  x-log "Theme '${theme}' set successfully."
+else
+  error="Failed to set theme '${theme}'."
+  x-log "$error" ERROR
+  rmpc remote --pid "$PID" status "$error" --level error
+fi
--- assets/.config/rmpc/on-song-change.sh
+++ assets/.config/rmpc/on-song-change.sh
@@ -2,12 +2,43 @@
 
 scripts_dir="$(realpath "$0" | xargs dirname)/on-song-change.d"
 
+x-log () {
+  local log_file=/tmp/rmpc.on-song-change.log
+  local log_dir="$(dirname "$log_file")"
+
+  local message="$1"
+  local level="$2"
+  local current_script="$(basename "$0")"
+
+  if [ -z "$level" ]; then
+    level="INFO"
+  fi
+
+  if [ -z "$message" ]; then
+    message="$level"
+    level="INFO"
+  fi
+
+  [ ! -d "$log_dir" ] && mkdir -p "$log_dir"
+  [ ! -f "$log_file" ] && touch "$log_file"
+
+  echo "[${level}] ${current_script} | ${message}" >> "$log_file"
+}
+
+# make the x-log function available to subshells
+export -f x-log
+
+x-log "ARTIST $ARTIST"
+x-log " ALBUM $ALBUM"
+x-log " TITLE $TITLE"
+
 for n in "$scripts_dir"/*.*; do
   if [[ "$(basename "$n")" == *.disabled ]]; then
-    echo "Skipping disabled script: $n" >> /tmp/rmpc-download-art.sh.log
+    x-log "Skipping disabled script: $n"
     continue
   fi
 
-  echo "Running script: $n" >> /tmp/rmpc-download-art.sh.log
+  x-log "Running script: $n"
+
   bash -c "$n" &
 done
